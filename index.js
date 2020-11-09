1a01309eef3f10829e5b296646f1eb26584d2b14
danj-O <danjowanjo@gmail.com>
2020-11-09 00:40:05 -0500

    fixing port issue for heroku deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -19,6 +20,6 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+http.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
